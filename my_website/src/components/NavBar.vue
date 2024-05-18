4797aaab561d9e8ff8152ba5cebbe2cfb06a6c83
keeganasmith <keeganasmith2003@gmail.com>
2024-05-18 14:47:28 -0700

    google oauth

--- my_website/src/components/NavBar.vue
+++ my_website/src/components/NavBar.vue
@@ -1,8 +1,12 @@
+<script setup>
+import GoogleSigin from './GoogleSignin.vue'
+</script>
 <template>
     <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/blog">Blog</RouterLink>
         <RouterLink to="/projects">Projects</RouterLink>
         <RouterLink to="/new_post">New Post</RouterLink>
+        <GoogleSigin />
     </nav>
 </template>
