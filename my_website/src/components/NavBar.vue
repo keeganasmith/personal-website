543beb780932b0246486b34cd02045bc934924e8
keeganasmith <keeganasmith2003@gmail.com>
2024-05-24 13:22:17 -0700

    add login button to navbar

--- my_website/src/components/NavBar.vue
+++ my_website/src/components/NavBar.vue
@@ -1,13 +1,10 @@
 <template>
     <div class="bg-gray-800">
         <nav class="container p-4 mx-auto md:flex md:justify-between md:items-center">
-            <div class="flex items-center justify-between">
-                <router-link to="/" class="text-white px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700">About Me
-                </router-link>
-                <!-- Mobile menu button -->
+            <div class="flex items-center justify-between w-full">
+                <router-link to="/" class="text-white px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700">About Me</router-link>
                 <div @click="toggleNav" class="flex md:hidden">
-                    <button type="button"
-                        class="text-gray-100 hover:text-gray-400 focus:outline-none focus:text-gray-400">
+                    <button type="button" class="text-gray-100 hover:text-gray-400 focus:outline-none focus:text-gray-400">
                         <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
                             <path fill-rule="evenodd"
                                 d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z">
@@ -17,15 +14,14 @@
                 </div>
             </div>
 
-            <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-            <ul :class="showMenu ? 'flex' : 'hidden'"
-                class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
+            <ul :class="showMenu ? 'flex' : 'hidden'" class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
                 <router-link to="/blog" class="text-white px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700">Blog</router-link>
                 <router-link to="/projects" class="text-white px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700">Projects</router-link>
                 <router-link to="/experience" class="text-white px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700">Work Experience</router-link>
                 <router-link to="/blocktd" class="text-white px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700">Block TD</router-link>
                 <router-link to="/contact" class="text-white px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700">Contact Me</router-link>
                 <router-link to="/new_post" v-if="is_admin" class="text-white px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700">New Post</router-link>
+                <router-link to="/login" class="text-white px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700">Login</router-link>
             </ul>
         </nav>
     </div>
